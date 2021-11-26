5317ca1bea7f94e9d4bac3f4302139ee5ff54496
Eder Sena <eder.sena@live.com>
2021-11-26 16:14:26 -0300

    Quasar configuração fino do fino

---
+++ quasar-todo/src/layouts/MainLayout.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="mdi-menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <q-btn
+          icon="mdi-logout"
+          flat
+          rounded
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-toolbar class="bg-primary" />
+        <q-item class="bg-grey-3">
+          <q-item-section>
+            Lists
+          </q-item-section>
+
+          <q-item-section>
+            <q-btn
+              flat
+              round
+              icon="mdi-plus"
+            />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ quasar-todo/src/pages/Index.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-page class="row">
+    <!-- <div
+      class="bg-blue col-3"
+      style="height: 22px"
+    />
+
+    <div
+      class="bg-purple col-3"
+      style="height: 22px"
+    />
+
+    <div
+      class="bg-green col-6"
+      style="height: 22px"
+    /> -->
+
+    <q-card class="col-xs-9">
+      <q-toolbar>
+        <q-input
+          placeholder="What needs to be search?"
+          dense
+          borderless
+          class="full-width"
+          :model-value="search"
+        >
+          <template #append>
+            <q-icon name="mdi-magnify" />
+          </template>
+        </q-input>
+      </q-toolbar>
+
+      <q-list bordered>
+        <q-item>
+          <q-item-section side>
+            <q-checkbox :model-value="true" />
+          </q-item-section>
+
+          <q-item-section>
+            Go Shopping
+          </q-item-section>
+
+          <q-item-section side>
+            <q-btn
+              flat
+              round
+              size="sm"
+              icon="mdi-delete"
+            />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card>
+
+    <q-card class="col-xs-3">
+      <q-toolbar class="bg-primary">
+        <q-btn
+          fab
+          color="blue-5"
+          class="q-ml-lg "
+          style="margin-bottom: -47px"
+          icon="mdi-plus"
+        />
+      </q-toolbar>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'PageIndex',
+  data () {
+    return {
+      search: ''
+    }
+  }
+})
+</script>
